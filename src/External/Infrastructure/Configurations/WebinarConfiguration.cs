8018ec5ea009b7433a5516fc33c554168dc4c6f9
Yami An <yamiannephilim@gmail.com>
2023-09-05 22:55:33 +0700

    Update WebinarConfiguration.cs

--- src/External/Infrastructure/Configurations/WebinarConfiguration.cs
+++ src/External/Infrastructure/Configurations/WebinarConfiguration.cs
@@ -8,9 +8,9 @@ public sealed class WebinarConfiguration : IEntityTypeConfiguration<Webinar>
 {
     public void Configure(EntityTypeBuilder<Webinar> builder)
     {
-        builder.ToTable("Webinars");
-        builder.HasKey(w => w.Id);
-        builder.Property(w => w.Name).HasMaxLength(100);
-        builder.Property(w => w.ScheduleOn).IsRequired();
+        _ = builder.ToTable("Webinars");
+        _ = builder.HasKey(w => w.Id);
+        _ = builder.Property(w => w.Name).HasMaxLength(100);
+        _ = builder.Property(w => w.ScheduleOn).IsRequired();
     }
 }
